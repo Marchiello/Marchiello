32fd62cc359135ec11f0a24adedb1707a924d9cb
Marchiello <99300987+Marchiello@users.noreply.github.com>
2024-08-26 16:06:04 -0300

    Delete Estudos directory

--- Estudos/Javascript/Javascript_Basico_CursoEmVideo/exercicios/ex011.js
+++
@@ -1,8 +0,0 @@
-var idade = 16
-if(idade < 18){
-    console.log("Não vota")
-}else if(idade < 18 || idade > 65){
-    console.log("Voto Opcional")
-}else{
-    console.log("Voto obrigatório")
-}
--- Estudos/Javascript/Javascript_Basico_CursoEmVideo/moduloF/funcao001.js
+++
@@ -1,9 +0,0 @@
-function parimpar(n){
-    if(n%2 == 0){
-        return 'par'
-    }else{
-        return 'impar'
-    }
-}
-
-console.log(parimpar(5))
